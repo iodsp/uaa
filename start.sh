6c00bcbb4e49c7ef4447fceb0cc426a6e82b27dc
nmred_2008 <nmred_2008@126.com>
2017-11-07 09:12:13 +0000

    add travis

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#set -e
+
+while true 
+do
+  sleep 1
+done
--- target/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar -Dlogging.path=/usr/local/adinf/logs -Ddebug=false -Dfehash.value=a3d67a6 iodsp-uaa-1.0-SNAPSHOT.jar
